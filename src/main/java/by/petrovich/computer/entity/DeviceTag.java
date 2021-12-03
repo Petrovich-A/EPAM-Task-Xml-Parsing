5f0347b231eee0c162d79ab35b15bcf54d824dc6
Alexandr <a.piatrovich@gmail.com>
2021-12-03 23:08:51 +0300

    fix DeviceTag

--- src/main/java/by/petrovich/computer/entity/DeviceTag.java
+++ src/main/java/by/petrovich/computer/entity/DeviceTag.java
@@ -16,5 +16,16 @@ public enum DeviceTag {
     CRITICAL,
 
             FREQUENCY,
-            VOLUME
+            VOLUME;
+
+    private static final String UNDERSCORE = "_";
+    private static final String HYPHEN = "-";
+
+    @Override
+    public String toString() {
+        String result = this.name();
+        result = result.toLowerCase();
+        result = result.replace(UNDERSCORE, HYPHEN);
+        return result;
+    }
 }
