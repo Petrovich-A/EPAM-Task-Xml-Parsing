6e5bb20bbec49bf704b9b44bd31a23e5959647fb
Petrovich-A <A.Piatrovich@gmail.com>
2021-12-03 14:31:46 +0300

    added XML validator

---
+++ src/main/java/by/petrovich/computer/parser/DeviceHandler.java
@@ -0,0 +1,7 @@
+package by.petrovich.computer.parser;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+public class DeviceHandler extends DefaultHandler {
+
+}
---
+++ src/main/java/by/petrovich/computer/validator/XmlValidator.java
@@ -0,0 +1,40 @@
+package by.petrovich.computer.validator;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class XmlValidator {
+    private static final Logger logger = LogManager.getLogger();
+
+    public XmlValidator() {
+    }
+
+    public boolean isFileValid(String xmlFilePath, String xsdFilePath) {
+        boolean result = true;
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI);
+
+        try {
+            Schema schema = schemaFactory.newSchema(new File(xsdFilePath));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(xmlFilePath)));
+        } catch (SAXException e) {
+            logger.log(Level.ERROR, "Fail with XML file reading: {}", xmlFilePath, e);
+            result = false;
+        } catch (IOException e) {
+            logger.log(Level.ERROR, "XML fail is not valid with XSD scheme: {}", xsdFilePath, xmlFilePath, e);
+            result = false;
+        }
+        return result;
+    }
+
+}
