68f0ce5dcf6f9547407d567a4819f48332ff671d
Alexandr <a.piatrovich@gmail.com>
2021-12-06 18:54:34 +0300

    some fixes in DeviceHandler

--- src/main/java/by/petrovich/computer/parser/DeviceHandler.java
+++ src/main/java/by/petrovich/computer/parser/DeviceHandler.java
@@ -24,6 +24,7 @@ public class DeviceHandler extends DefaultHandler {
     private DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.US);
     private static final char HYPHEN = '-';
     private static final char UNDERSCORE = '_';
+    private static final char SPACE = ' ';
     private static final Logger logger = LogManager.getLogger();
     private final List<DeviceAbstract> devices;
     private DeviceAbstract currentDevice;
@@ -48,7 +49,7 @@ public class DeviceHandler extends DefaultHandler {
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) {
         String name = qName.toUpperCase(Locale.ROOT).replace(HYPHEN, UNDERSCORE);
-        if (qName.equals(DeviceType.CPU.toString()) || qName.equals(DeviceType.CPU.toString())) {
+        if (qName.equals(DeviceType.CPU.toString()) || qName.equals(DeviceType.HDD.toString())) {
             DeviceType deviceType = DeviceType.valueOf(name);
             switch (deviceType) {
                 case CPU:
@@ -63,6 +64,7 @@ public class DeviceHandler extends DefaultHandler {
             }
             if (attributes.getLength() == 1) {
                 currentDevice.setDeviceId(attributes.getValue(0));
+                logger.info("startElement: DeviceId: " + attributes.getValue(0));
                 currentDevice.setDeviceId(DeviceAbstract.DEFAULT_PICTURE);
             } else {
                 int idAttributeIndex = attributes.getLocalName(0).equals(DeviceTag.ID.toString()) ? 0 : 1;
@@ -76,7 +78,7 @@ public class DeviceHandler extends DefaultHandler {
                 currentTag = deviceTag;
             }
         }
-        logger.info("DeviceHandler: startElement: " + name.trim());
+        logger.info("DeviceHandler: " + name.trim());
     }
 
     @Override
@@ -100,42 +102,49 @@ public class DeviceHandler extends DefaultHandler {
             switch (currentTag) {
                 case NAME:
                     currentDevice.setName(data);
-                    logger.info("characters: data: " + data);
+                    logger.info("characters: " + data);
                     break;
                 case DELIVERY_DATE:
                     String deliveryDate = data.toUpperCase(Locale.ROOT);
                     currentDevice.setDeliveryDate(LocalDate.parse(deliveryDate, DATE_FORMATTER));
-                    logger.info("characters: data: " + data + "\n deliveryDate: " + deliveryDate );
+                    logger.info("characters: " + data);
                     break;
                 case COUNTRY:
                     String country = data.toUpperCase(Locale.ROOT);
                     currentDevice.setCountry(Country.valueOf(country));
+                    logger.info("characters: " + data);
                     break;
                 case PRICE:
                     String price = data.toUpperCase(Locale.ROOT);
                     currentDevice.setPrice(Double.parseDouble(price));
+                    logger.info("characters: " + data);
                     break;
                 case PERIPHERAL:
-                    String peripheral = data.toUpperCase(Locale.ROOT).replace(HYPHEN, UNDERSCORE);
+                    String peripheral = data.toUpperCase(Locale.ROOT).replace(SPACE, UNDERSCORE);
                     currentDevice.setPeripheral(Peripheral.valueOf(peripheral));
+                    logger.info("characters: " + data);
                     break;
                 case PORT:
                     String port = data.toUpperCase(Locale.ROOT);
                     currentDevice.setPort(port);
+                    logger.info("characters: " + data);
                     break;
                 case CRITICAL:
                     String critical = data.toUpperCase(Locale.ROOT);
                     currentDevice.setCritical(Boolean.parseBoolean(critical));
+                    logger.info("characters: " + data);
                     break;
                 case FREQUENCY:
                     Cpu cpu = (Cpu) currentDevice;
                     String frequency = data.toUpperCase(Locale.ROOT);
                     cpu.setSequanse(Integer.parseInt(frequency));
+                    logger.info("characters: " + data);
                     break;
                 case VOLUME:
                     Hdd hdd = (Hdd) currentDevice;
                     String volume = data.toUpperCase(Locale.ROOT);
                     hdd.setVolume(Integer.parseInt(volume));
+                    logger.info("characters: " + data);
                     break;
                 default:
                     throw new EnumConstantNotPresentException(currentTag.getDeclaringClass(), currentTag.name());
