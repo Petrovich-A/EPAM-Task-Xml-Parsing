7d3727319fdb9088f0687e43be4732503ea89986
Petrovich-A <A.Piatrovich@gmail.com>
2021-12-07 15:35:55 +0300

    added setDeviceProperty

--- src/main/java/by/petrovich/computer/parser/DeviceStaxBuilder.java
+++ src/main/java/by/petrovich/computer/parser/DeviceStaxBuilder.java
@@ -4,7 +4,9 @@ import by.petrovich.computer.entity.Cpu;
 import by.petrovich.computer.entity.DeviceAbstract;
 import by.petrovich.computer.entity.DeviceTag;
 import by.petrovich.computer.entity.Hdd;
+import by.petrovich.computer.entity.type.Country;
 import by.petrovich.computer.entity.type.DeviceType;
+import by.petrovich.computer.entity.type.Peripheral;
 import by.petrovich.computer.exception.DeviceException;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -17,11 +19,14 @@ import javax.xml.stream.XMLStreamReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class DeviceStaxBuilder implements DeviceBuilder {
+    private DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.US);
     private static final Logger logger = LogManager.getLogger();
     private static final char HYPHEN = '-';
     private static final char UNDERSCORE = '_';
@@ -62,7 +67,7 @@ public class DeviceStaxBuilder implements DeviceBuilder {
 
     @Override
     public List<DeviceAbstract> getDevices() {
-        return devices;
+        return List.copyOf(devices);
     }
 
     private DeviceAbstract devicesBuild(XMLStreamReader xmlStreamReader) throws XMLStreamException {
@@ -96,8 +101,72 @@ public class DeviceStaxBuilder implements DeviceBuilder {
                         return deviceAbstract;
                     }
                 }
-
             }
         }
+        throw new XMLStreamException("Unknown element in tag");
     }
+
+    private void setDeviceProperty(XMLStreamReader xmlStreamReader, String name, DeviceAbstract deviceAbstract) throws XMLStreamException {
+        switch (DeviceTag.valueOf(name)) {
+            case NAME:
+                String deviceName = getXmlText(xmlStreamReader);
+                deviceAbstract.setName(deviceName);
+                logger.info("setDeviceProperty: " + deviceName);
+                break;
+            case DELIVERY_DATE:
+                String deliveryDate = getXmlText(xmlStreamReader);
+                deviceAbstract.setDeliveryDate(LocalDate.parse(deliveryDate, DATE_FORMATTER));
+                logger.info("setDeviceProperty: " + deliveryDate);
+                break;
+            case COUNTRY:
+                String country = getXmlText(xmlStreamReader);
+                deviceAbstract.setCountry(Country.valueOf(country));
+                logger.info("setDeviceProperty: " + country);
+                break;
+            case PRICE:
+                String price = getXmlText(xmlStreamReader);
+                deviceAbstract.setPrice(Double.parseDouble(price));
+                logger.info("setDeviceProperty: " + price);
+                break;
+            case PERIPHERAL:
+                String peripheral = getXmlText(xmlStreamReader);
+                deviceAbstract.setPeripheral(Peripheral.valueOf(peripheral));
+                logger.info("setDeviceProperty: " + peripheral);
+                break;
+            case PORT:
+                String port = getXmlText(xmlStreamReader);
+                deviceAbstract.setPort(port);
+                logger.info("setDeviceProperty: " + port);
+                break;
+            case CRITICAL:
+                String critical = getXmlText(xmlStreamReader);
+                deviceAbstract.setCritical(Boolean.parseBoolean(critical));
+                logger.info("setDeviceProperty: " + critical);
+                break;
+            case FREQUENCY:
+                Cpu cpu = (Cpu) deviceAbstract;
+                String frequency = getXmlText(xmlStreamReader);
+                cpu.setFrequency(Integer.parseInt(frequency));
+                logger.info("setDeviceProperty: " + frequency);
+                break;
+            case VOLUME:
+                Hdd hdd = (Hdd) deviceAbstract;
+                String volume = getXmlText(xmlStreamReader);
+                hdd.setVolume(Integer.parseInt(volume));
+                logger.info("setDeviceProperty: " + volume);
+                break;
+            default:
+//                throw new EnumConstantNotPresentException(deviceAbstract.getDeclaringClass(), currentTag.name());
+        }
+    }
+
+    private String getXmlText(XMLStreamReader xmlStreamReader) throws XMLStreamException {
+        String text = null;
+        if (xmlStreamReader.hasNext()) {
+            xmlStreamReader.next();
+            text = xmlStreamReader.getText();
+        }
+        return text;
+    }
+
 }
